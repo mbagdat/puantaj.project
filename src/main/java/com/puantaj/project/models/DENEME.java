159d994431d33c21a02757bc0719cad5abc39a9b
MutluB <bagdatmutlu@gmail.com>
2023-07-03 23:07:57 +0300

    test

---
+++ src/main/java/com/puantaj/project/models/DENEME.java
@@ -0,0 +1,29 @@
+package com.puantaj.project.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class DENEME {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+}
